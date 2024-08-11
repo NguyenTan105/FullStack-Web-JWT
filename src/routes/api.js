f8f25460b2578cd4f4981f40d3cf85dde19b30da
NguyenTan105 <tannguyenkhacminh112@gmail.com>
2024-08-11 19:50:31 +0700

    initial 8/11/2024

---
+++ src/routes/api.js
@@ -0,0 +1,20 @@
+const { resolveInclude } = require("ejs");
+const express = require("express");
+
+const routerAPI = express.Router();
+
+// const { getUsersAPI, postCreateUserAPI,
+//     putUpdateUserAPI, deleteUserAPI
+
+// } = require('../controllers/apiController')
+
+// routerAPI.get('/users', getUsersAPI);
+// routerAPI.post('/users', postCreateUserAPI);
+// routerAPI.put('/users', putUpdateUserAPI);
+// routerAPI.delete('/users', deleteUserAPI);
+
+routerAPI.get("/", (req, res) => {
+  return res.status(200).json("Hello NguyenTan");
+});
+
+module.exports = routerAPI; //export default
